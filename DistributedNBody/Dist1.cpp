f522af7c0eb9a881591b69c827e31f4fc2a3390b
zkhatami88 <z.khatami88@gmail.com>
2015-09-15 14:13:10 -0500

    commit 2

--- DistributedNBody/Dist1.cpp
+++ DistributedNBody/Dist1.cpp
@@ -98,97 +98,6 @@ private:
 };
 
 ///////////////////////////////////////////////////////////////////////////////
-/*
-struct partition_data
-{
-private:
-    typedef hpx::serialization::serialize_buffer<double> buffer_type;
-
-    struct hold_reference
-    {
-        hold_reference(buffer_type const& data)
-                : data_(data)
-        {}
-
-        void operator()(double*) {}     // no deletion necessary
-
-        buffer_type data_;
-    };
-
-    static void deallocate(double* p)
-    {
-        alloc_.deallocate(p);
-    }
-
-    static partition_allocator<double> alloc_;
-
-public:
-    partition_data()
-            : size_(0)
-    {}
-
-    // Create a new (uninitialized) partition of the given size.
-    partition_data(std::size_t size)
-            : data_(alloc_.allocate(size), size, buffer_type::take,
-                    &partition_data::deallocate),
-              size_(size),
-              min_index_(0)
-    {}
-
-    // Create a new (initialized) partition of the given size.
-    partition_data(std::size_t size,std::size_t size_mem, int cell_IDs, int flag)
-            : data_(alloc_.allocate(size), size, buffer_type::take,
-                    &partition_data::deallocate),
-              size_(size),
-              min_index_(0)
-   {
-        for(std::size_t j=0; j<size; ++j)
-                data_[j]=double(cell_IDs);
-    }
-    // Create a partition which acts as a proxy to a part of the embedded array.
-    // The proxy is assumed to refer to either the left or the right boundary
-    // element.
-    partition_data(partition_data const& base, std::size_t min_index)
-            : data_(base.data_.data()+min_index, 1, buffer_type::reference,
-                    hold_reference(base.data_)),      // keep referenced partition alive
-              size_(base.size()),
-              min_index_(min_index)
-    {
-        HPX_ASSERT(min_index < base.size());
-    }
-
-    double& operator[](std::size_t idx) { return data_[index(idx)]; }
-    double operator[](std::size_t idx) const { return data_[index(idx)]; }
-
-    std::size_t size() const { return size_; }
-
-private:
-    std::size_t index(std::size_t idx) const
-    {
-        HPX_ASSERT(idx >= min_index_ && idx < size_);
-        return idx - min_index_;
-    }
-
-private:
-    // Serialization support: even if all of the code below runs on one
-    // locality only, we need to provide an (empty) implementation for the
-    // serialization as all arguments passed to actions have to support this.
-    friend class hpx::serialization::access;
-
-    template <typename Archive>
-    void serialize(Archive& ar, const unsigned int version)
-    {
-        ar & data_ & size_ & min_index_;
-    }
-
-private:
-    buffer_type data_;
-    std::size_t size_;
-    std::size_t min_index_;
-};
-
-partition_allocator<double> partition_data::alloc_;
-*/
 
 struct partition_data
 {
@@ -1156,21 +1065,14 @@ stepper_server::space stepper_server::do_work(std::size_t np, std::size_t nl)
         space & From_B= Parent_[(time + 1) % 2]; space & From_F= Parent_[(time + 1) % 2];
         space & From_L= Parent_[(time + 1) % 2]; space & From_R= Parent_[(time + 1) % 2];
         space & From_U= Parent_[(time + 1) % 2]; space & From_D= Parent_[(time + 1) % 2];
-/*
+
         From_B[0] = dataflow(hpx::launch::async, &stepper_server::New_Members, B[0], receive_B(time), ID_lc);
         From_F[0] = dataflow(hpx::launch::async, &stepper_server::New_Members, F[0], receive_F(time), ID_lc);
         From_L[0] = dataflow(hpx::launch::async, &stepper_server::New_Members, L[0], receive_L(time), ID_lc);
         From_R[0] = dataflow(hpx::launch::async, &stepper_server::New_Members, R[0], receive_R(time), ID_lc);
         From_U[0] = dataflow(hpx::launch::async, &stepper_server::New_Members, U[0], receive_U(time), ID_lc);
         From_D[0] = dataflow(hpx::launch::async, &stepper_server::New_Members, D[0], receive_D(time), ID_lc);
-*/
 
-        From_B[0] = dataflow(hpx::launch::async, &stepper_server::New_Members, B[0], B[0], ID_lc);
-        From_F[0] = dataflow(hpx::launch::async, &stepper_server::New_Members, F[0], F[0], ID_lc);
-        From_L[0] = dataflow(hpx::launch::async, &stepper_server::New_Members, L[0], L[0], ID_lc);
-        From_R[0] = dataflow(hpx::launch::async, &stepper_server::New_Members, R[0], R[0], ID_lc);
-        From_U[0] = dataflow(hpx::launch::async, &stepper_server::New_Members, U[0], U[0], ID_lc);
-        From_D[0] = dataflow(hpx::launch::async, &stepper_server::New_Members, D[0], D[0], ID_lc);
 
         for(std::size_t j=0; j<local_np; ++j) {
             int i = cell_IDs[j];
